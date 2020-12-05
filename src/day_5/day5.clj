615de7d3d578f0b5b2d2af09add8286416a77d20
John Herrlin <john.herrlin@fortnox.se>
2020-12-05 21:39:27 +0100

    day5 done

--- src/day_5/day5.clj
+++ src/day_5/day5.clj
@@ -7,21 +7,48 @@
   (println "Top:" (seq coll) begin end)
   (cond
     (and (= 1 (count coll))
-         (= \B (first coll)))
-    (dec (+ begin (/ (- end begin) 2)))
+         (#{\B \R} (first coll)))
+    (+ begin (/ (- end begin) 2))
 
     (and (= 1 (count coll))
-         (= \F (first coll)))
+         (#{\F \L} (first coll)))
     (dec (+ (/ end 2) (/ begin 2)))
 
-    (= \B (first coll))
+    (#{\B \R} (first coll))
     (let [b (+ begin (/ (- end begin) 2))
           e end]
       (println (first coll) b e)
       (find-row (rest coll) b e))
 
-    (= \F (first coll))
+    (#{\F \L} (first coll))
     (let [b begin
           e (+ (/ end 2) (/ begin 2))]
       (println (first coll) b e)
       (find-row (rest coll) b e))))
+
+
+(defn find-it [s]
+  (let [[row column] (map
+                      (partial clojure.string/join "")
+                      (split-at 7 s))]
+    ;; (find-row row 0 128)
+    (+ (* (find-row row 0 128) 8)
+       (find-row column 0 8))))
+
+(->> (clojure.string/split (slurp "src/day_5/input.txt") #"\n")
+       (map find-it)
+       (sort))
+
+;; That's not the right answer; your answer is too low. If you're stuck, make
+;; sure you're using the full input data; there are also some general tips on
+;; the about page, or you can ask for hints on the subreddit. Please wait one
+;; minute before trying again. (You guessed 122.) [Return to Day 5]
+
+
+(def a (->> (clojure.string/split (slurp "src/day_5/input.txt") #"\n")
+           (map find-it)
+           (set)))
+
+(def b (set (range 13 978)))
+
+(clojure.set/difference b a)
--- test/day_5/day5_test.clj
+++ test/day_5/day5_test.clj
@@ -13,4 +13,22 @@
   (t/is (= (sut/find-row "BBFFBBF" 0 128)
            102))
 
+  (t/is (= (sut/find-row "RRR" 0 8)
+           7))
+
+  (t/is (= (sut/find-row "LLL" 0 8)
+           0))
+
+  (t/is (= (sut/find-row "RLL" 0 8)
+           4))
+
+  (t/is (= (sut/find-it "BFFFBBFRRR")
+           567))
+
+  (t/is (= (sut/find-it "FFFBBBFRRR")
+           119))
+
+  (t/is (= (sut/find-it "BBFFBBFRLL")
+           820))
+
   )
