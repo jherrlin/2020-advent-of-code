6a8fa371253c9d86416f3ac74e47b55b3b2ce323
John Herrlin <jherrlin@gmail.com>
2020-12-09 19:27:22 +0100

    day 9 cleanup

--- src/day_9/day9.clj
+++ src/day_9/day9.clj
@@ -1,31 +1,8 @@
 (ns day-9.day9)
 
-(def test-input "35
-20
-15
-25
-47
-40
-62
-55
-65
-95
-102
-117
-150
-182
-127
-219
-299
-277
-309
-576")
 
-
-
-
-(def values
-  (->> (clojure.string/split #_test-input (slurp "src/day_9/input.txt") #"\n")
+(def input-values
+  (->> (clojure.string/split (slurp "src/day_9/input.txt") #"\n")
        (mapv #(Long/valueOf %))))
 
 (defn calc-window-values [window-values]
@@ -34,29 +11,26 @@
              :when (not (= x y))]
          (+ x y))))
 
-(defn calculate [all-values window-start window-end]
-  (let [value (nth all-values (inc window-end))
-        window (subvec all-values window-start (inc window-end))]
-    (if-not (contains? (calc-window-values window) value)
+(defn fist-number [input-values window-start window-end]
+  (let [value  (nth input-values (inc window-end))
+        window (subvec input-values window-start (inc window-end))]
+    (if-not ((calc-window-values window) value)
       value
-      (calculate all-values (inc window-start) (inc window-end)))))
+      (fist-number input-values (inc window-start) (inc window-end)))))
 
 ;; First
 (def first-finding
-  (calculate values 0 25))
+  (fist-number input-values 0 25))
 
-(defn find-a-contiguous-set [all-values value-to-find window-start window-end window-size]
-  (when (< window-end (count all-values))
-    (let [window (subvec all-values window-start window-end)]
+(defn find-a-contiguous-set [input-values value-to-find window-start window-end window-size]
+  (when (< window-end (count input-values))
+    (let [window (subvec input-values window-start window-end)]
       (if (#{value-to-find} (reduce + window))
         (->> window ((juxt (partial apply min) (partial apply max))) (apply +))
-        (find-a-contiguous-set all-values value-to-find (inc window-start) (inc window-end) window-size)))))
+        (find-a-contiguous-set input-values value-to-find (inc window-start) (inc window-end) window-size)))))
 
-(map (fn [window-size]
-       (find-a-contiguous-set
-        values
-        first-finding
-        0
-        window-size
-        window-size))
-     (range 2 (count values)))
+;; Second
+(->> (range 2 (count input-values))
+     (map #(find-a-contiguous-set input-values first-finding 0 % %))
+     (remove nil?)
+     (first))
--- test/day_9/day9_test.clj
+++ test/day_9/day9_test.clj
@@ -2,6 +2,21 @@
   (:require [day-9.day9 :as sut]
             [clojure.test :as t]))
 
-(t/deftest calculate
-  (t/is (= (sut/calculate sut/values 0 25)
+(t/deftest fist-number
+  (t/is (= (sut/fist-number sut/input-values 0 25)
            1212510616)))
+
+
+(t/deftest problem-nr-2
+  (t/is (= (->> (map (fn [window-size]
+                       (sut/find-a-contiguous-set
+                        sut/input-values
+                        1212510616
+                        0
+                        window-size
+                        window-size))
+                     (range 2 (count sut/input-values)))
+                (remove nil?)
+                (first))
+           171265123))
+  )
